80037704ba07c13c1492afc7bb0b3d29857dbdec
Andrew Bannerman <andrewbannerman@criptext.com>
2022-03-29 21:11:48 -0500

    Update 1491. Average Salary Excluding the Minimum and Maximum Salary.cpp

--- 1491. Average Salary Excluding the Minimum and Maximum Salary.cpp
+++ 1491. Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -1,4 +1,14 @@
-/* 1491. Average Salary Excluding the Minimum and Maximum Salary */
+/* 1491. Average Salary Excluding the Minimum and Maximum Salary 
+
+You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
+
+Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted. 
+
+Constraints:
+
+3 <= salary.length <= 100
+1000 <= salary[i] <= 106
+All the integers of salary are unique. */
 
 /* 43 / 43 test cases passed.
 Status: Accepted
